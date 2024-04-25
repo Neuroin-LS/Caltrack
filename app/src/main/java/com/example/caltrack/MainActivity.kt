b46a0cb0c6a423e3ec2349014a1ef91944e6164d
Danendra <ft.nendra@gmail.com>
2024-04-25 10:42:11 +0700

    Initial commit

---
+++ app/src/main/java/com/example/caltrack/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.caltrack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import android.widget.PopupMenu
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.lang.Exception
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var bottomNav : BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        loadFragment(FoodFragment())
+        bottomNav = findViewById(R.id.bottomNav) as BottomNavigationView
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.person -> {
+                    loadFragment(PersonFragment())
+                    true
+                }
+                R.id.kcal -> {
+                    loadFragment(KcalFragment())
+                    true
+                }
+                R.id.food -> {
+                    loadFragment(FoodFragment())
+                    true
+                }
+                R.id.bmi -> {
+                    loadFragment(BmiFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.commit()
+    }
+
+}
